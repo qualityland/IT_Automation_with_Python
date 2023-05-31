08d7669152e14645a8624beb3e7593e0ad0fc942
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2023-05-31 11:02:51 +0200

    course 2, week 2 started

--- 02_interact_with_OS/week_01/01_import_module.py
+++ 02_interact_with_OS/week_01/01_import_module.py
@@ -4,3 +4,12 @@ import arrow
 
 date = arrow.get('2020-01-18', 'YYYY-MM-DD')
 print(date.shift(weeks=+6).format("MMM DD YYYY"))
+
+import pandas
+
+visitors = [1235, 6424, 9345, 8464, 2345]
+errors = [23, 45, 33, 45, 76]
+df = pandas.DataFrame({"visitors": visitors, "errors": errors}, index=["Mon", "Tue", "Wed", "Thu", "Fri"])
+print(df)
+print(df["errors"].mean())
+print(df["visitors"].mean())
